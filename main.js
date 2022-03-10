84ec249edf4c51434ce945802806bda0c9380b09
leightonmanning <leightonmanning@hotmail.co.uk>
2022-03-10 17:45:53 +0000

    initial commit and test

---
+++ main.js
@@ -0,0 +1,9 @@
+// const apiKey = "10e7f5c"
+// let filmTitle = "Shrek"
+
+
+// const getFilm = async () => {
+//     const response = await fetch('http://www.omdbapi.com/?apikey='+apiKey+'&s='+filmTitle);
+//     const myJson = await response.json(); //extract JSON from the http response
+//     // do something with myJson
+//   }
